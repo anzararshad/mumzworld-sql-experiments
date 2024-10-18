c179e1a4e7979b5f7ba585ac3f46105bc400fc05
A.A.Ahamed Arshad <anzanfas@gmail.com>
2024-10-18 16:25:05 +0400

    Update pizza-1.sql

--- pizza-1.sql
+++ pizza-1.sql
@@ -1,5 +1,5 @@
 -- This SQL query fetches the number of pizzas ordered by customers who clearly have great taste!
 SELECT COUNT(*) AS "Pizza Lovers" 
 FROM orders 
-WHERE item = 'Pizza' 
+WHERE item = 'Pizza' and item = 'Cakes'
 AND customer_id IN (SELECT customer_id FROM customers WHERE taste = 'Exquisite');
